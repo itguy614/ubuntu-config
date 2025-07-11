c972eb636a4382bf15fadcca751696e90c13a3b1
Kurt Wolf <kwolf@gohcl.com>
2025-07-11 13:08:50 -0400

    wip

--- run.sh
+++ run.sh
@@ -13,22 +13,22 @@ apt upgrade -y
 
 # Define an array of scripts to run
 SCRIPTS=(
-    "scripts/configure-firewall.sh"
-    "scripts/configure-fail2ban.sh"
-    "scripts/configure-sshd.sh"
-    "scripts/configure-autoupdate.sh"
-    "scripts/configure-passwordpolicy.sh"
     "scripts/configuer-auditd.sh"
-    "scripts/configure-forkbombs.sh"
-    "scripts/configure-cron.sh"
+    "scripts/configure-autoupdate.sh"
     "scripts/configure-chrony.sh"
+    "scripts/configure-cron.sh"
+    "scripts/configure-fail2ban.sh"
+    "scripts/configure-firewall.sh"
+    "scripts/configure-forkbombs.sh"
+    "scripts/configure-issue.sh"
     "scripts/configure-logrotate.sh"
     "scripts/configure-motd.sh"
-    "scripts/configure-issue.sh"
-    "scripts/tcp-harden.sh"
-    "scripts/tcp-tune.sh"
+    "scripts/configure-passwordpolicy.sh"
+    "scripts/configure-sshd.sh"
     "scripts/file-tune.sh"
     "scripts/kernel-harden.sh"
+    "scripts/tcp-harden.sh"
+    "scripts/tcp-tune.sh"
 )
 
 for script in "${SCRIPTS[@]}"; do
